90f8a3cca7b319691ff1c0871cb5fc1362245acf
wochowskaj83 <wochowskaj@gmail.com>
2023-11-28 21:04:49 +0100

    dodawanie postów

---
+++ src/components/AddPost.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import './AddPost.css';
+import axios from 'axios';
+import { logDOM } from '@testing-library/react';
+
+const AddPost = (props) => {
+
+    const [postContent, setPostContent] = useState("");
+
+    const addPost = (e) => {
+        e.preventDefault();
+
+        if(!postContent) {
+            return;
+        }
+        
+        axios.post("https://akademia108.pl/api/social-app/post/add", {
+            content: postContent
+        })
+        .then((res) => {
+            props.getPrevPosts()
+            setPostContent('');
+
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    }
+
+    return (
+        <form className="addPostForm" onSubmit={addPost}>
+            <textarea placeholder="Add post..." onChangeCapture={(e) => setPostContent(e.target.value)} value={postContent}
+            ></textarea>
+            <button className="btn">Add</button>
+        </form>
+    );
+};
+
+export default AddPost;
--- src/views/Home.js
+++ src/views/Home.js
@@ -2,6 +2,7 @@ import './Home.css';
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Post from "../components/Post";
+import AddPost from '../components/AddPost';
 
 
 
@@ -13,6 +14,19 @@ const Home = (props) => {
             .post("https://akademia108.pl/api/social-app/post/latest")
             .then((res) => {
                 setPosts(res.data)
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+    const getPrevPosts = () => {
+        axios
+            .post("https://akademia108.pl/api/social-app/post/newer-then", {
+                date: posts[0].created_at,
+            })
+            .then((res) => {
+                setPosts(res.data.concat(posts));
             })
             .catch((error) => {
                 console.error(error);
@@ -38,6 +52,7 @@ const Home = (props) => {
 
     return (
         <div className="home">
+            {props.user && <AddPost getPrevPosts={getPrevPosts}/>}
             <div className="postList">
                 {posts.map((post) => {
                     return <Post post={post} key={post.id} />;
